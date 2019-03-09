8e74361a8c5b366add822d8600c01e3a6dd8ac79
et-taousy <zouhirtaousy@gmail.com>
2019-03-09 13:10:42 +0100

    first commit

---
+++ data_cleaning/root_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class root_appConfig(AppConfig):
+    name = 'root_app'
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Desktop/Master_aix_Marseille_cours/m2/PFE_Univ/final_prj/tutorial/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
